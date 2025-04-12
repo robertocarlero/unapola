82697d9274309bc8a4e88b7359803b1e6f9a8ab3
Roberto Carlos Quintero <robertocarlero@gmail.com>
2025-04-12 15:12:05 -0500

    fix(frontend): :bug: Correct html language setted

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
         className={`${poppinsSans.className} ${geistMono.variable} antialiased`}
       >
